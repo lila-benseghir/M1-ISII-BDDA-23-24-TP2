c739de4ad628b9529a6e32ee4414d844e74fdd75
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2024-04-30 18:22:15 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -6,6 +6,14 @@ GO
 USE UniversityDB
 
 -- Create the Tables
+create table laboratoire (
+  codelab varchar(4) primary key not null,
+  nomlab varchar(100), 
+  cheflab INT, 
+  datecréation date,
+  siteweb varchar(100)
+  );
+  
 create table projet (
   codeprj varchar(4) primary key not null, 
   datedebut date,
