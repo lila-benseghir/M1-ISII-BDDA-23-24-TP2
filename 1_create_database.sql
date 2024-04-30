258900d3ff12c5b8fe9993a422f7fc10629b23be
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2024-04-30 18:03:19 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,8 +1,16 @@
 -- Create a new database 'UniversityDB'
-
+create database UniversityDB
 
 -- Use the UniversityDB Database
 GO
 USE UniversityDB
 
 -- Create the Tables
+create table projet (
+  codeprj varchar(4) primary key not null, 
+  datedébut date,
+  datefin date,
+  responsable varchar(30), 
+  budget INT,
+  typeprj varchar(10)
+  );
