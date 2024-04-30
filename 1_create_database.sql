7440898ddd9bc21d4e6fa1a251c0cf9672a74eef
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2024-04-30 19:07:25 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -7,7 +7,7 @@ USE UniversityDB
 
 -- Create the Tables
   create table université ( codeuniversité varchar(10) primary key not null check ( codeuniversité in ('USTHB', 'UMBB' , 'USDB' , 'UAT' , 'ESI' ))  , nomuniversité varchar(60));
-create table typeprojet ( typeprj varchar(10) primary key not null check ( typeprj in ('PNR', 'CNEPRU' , 'AC' , 'CI' ))  , libellé varchar(60));
+create table typeprojet ( typeprj varchar(10) primary key not null check ( typeprj in ('PNR', 'CNEPRU' , 'AC' , 'CI' ))  , libelle varchar(60));
 create table Chercheur (numch INT primary key not null,nomch varchar(30), codelab  varchar(4)  ,codeuniversité varchar(10));
 create table participe (numch INT check (numch <9 )  , codeprj varchar(4) check ( codeprj in ('p1','p2','p3','p4','p5','p6','p7','p8','p9','p10','p11','p12','p13','p14','p15')),charghoraire INT,constraint clep_participe primary key (numch,codeprj));
 
