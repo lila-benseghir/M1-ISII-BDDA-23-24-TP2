0a1a04c51a72469e4720bb6cf7d96fc540ee9c6c
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2024-05-07 21:14:50 +0100

    Update 3_3_sort.sql

--- 3_3_sort.sql
+++ 3_3_sort.sql
@@ -1,3 +1,1 @@
-USE UniversityDB
 
-select codelab,sum (Charge_Horaire)  from chercheur inner join  participe on chercheur.numch = participe.numch group by codelab order by sum (Charge_Horaire) desc;
