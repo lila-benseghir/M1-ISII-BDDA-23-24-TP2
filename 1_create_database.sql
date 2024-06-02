ca83f02547fc34711c9006db33cc69cf7a4bf22e
lila-benseghir <151053365+lila-benseghir@users.noreply.github.com>
2024-06-02 18:07:42 +0200

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,62 +1,48 @@
--- Create UniversityDB Database
 CREATE DATABASE UniversityDB;
 GO
 
--- Use UniversityDB Database
 USE UniversityDB;
-GO
 
--- Create Universite Table
-CREATE TABLE universite (
-    codeuniversite VARCHAR(10) PRIMARY KEY,
-    nomuniversite VARCHAR(60)
+CREATE TABLE Laboratoire (
+    CodeLab VARCHAR(4) PRIMARY KEY,
+    NomLab VARCHAR(100),
+    cheflab INT,
+    Datcreation DATE,
+    Siteweb NVARCHAR(255)
 );
 
-GO
--- Create typeprojet Table
-CREATE TABLE typeprojet (
-    typeprj VARCHAR(10) PRIMARY KEY,
-    libelle VARCHAR(60)
-);
-GO 
--- Create Projet Table
-CREATE TABLE projet (
-    codeprj VARCHAR(4) PRIMARY KEY,
-    datedebut DATE,
-    datefin DATE,
-    responsable VARCHAR(30),
-    budget INT,
-    typeprj VARCHAR(10)
-);
-GO    
-
--- Create Chercheur Table
-CREATE TABLE chercheur (
-    numch int PRIMARY KEY ,
-    nomch varchar(30),
-    codelab varchar(4),
-    codeuniversite varchar(10)
-);
-    
-   
--- Create Laboratoire Table
-CREATE TABLE laboratoire (
-    codelab VARCHAR(4) PRIMARY KEY,
-    nomlab VARCHAR(100),
-    numch INT,
-    datcreation DATE,
-    siteweb NVARCHAR(255)
+CREATE TABLE Chercheur (
+    NumCh INT PRIMARY KEY,
+    NomCh VARCHAR(30),
+    CodeLab VARCHAR(4),
+    CodeUniversite VARCHAR(10)
 );
 
+CREATE TABLE Projet (
+    CodePrj VARCHAR(4) PRIMARY KEY,
+    DateDebut DATE,
+    DateFin DATE,
+    Responsable VARCHAR(30),
+    Budget INT,
+    TypePrj VARCHAR(10)
+);
 
-GO
+-- Create TypeProjet table
+CREATE TABLE TypeProjet (
+    TypePrj VARCHAR(10) PRIMARY KEY,
+    Libelle VARCHAR(60)
+);
 
-
--- Create Participe Table
-CREATE TABLE participe (
-    numch INT,
-    codeprj varchar(4),
+-- Create Participe table
+CREATE TABLE Participe (
+    NumCh INT,
+    CodePrj VARCHAR(4),
     Charge_Horaire INT,
-    PRIMARY KEY (numch, codeprj)
+    PRIMARY KEY (NumCh, CodePrj)
 );
-GO
+
+-- Create Universite table
+CREATE TABLE Universite (
+    CodeUniversite VARCHAR(10) PRIMARY KEY,
+    NomUniversite VARCHAR(60)
+);
